a6c8bb14133fbb812b7565473753a07abe7b415a
Maximilian Ring <mring@student.42heilbronn.de>
2025-04-11 16:29:52 +0200

    sync

--- philo/main.c
+++ philo/main.c
@@ -6,20 +6,49 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:17:54 by mring             #+#    #+#             */
-/*   Updated: 2025/04/10 15:49:56 by mring            ###   ########.fr       */
+/*   Updated: 2025/04/11 15:47:42 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-#include <stdio.h>
+
+void	parse_pre_init(char **av)
+{
+	int	test;
+
+	test = ft_atoi(av[2]);
+	if (test == -1)
+	{
+		printf("atoi parsing\n");
+		exit(1);
+	}
+	printf("%d\n", test);
+}
 
 int	main(int ac, char **av)
 {
-	if (ac != 5)
+	if (ac == 5 || ac == 6)
+	{
+		parse_pre_init(av);
+	}
+	else
 	{
 		printf("Usage: \
-			%s number_of_philos time_to_die time_to_eat time_to_sleep [meal_amount]\n",
+	%s number_of_philos time_to_die time_to_eat time_to_sleep [meal_amount]\n",
 				av[0]);
 		return (1);
 	}
+	// parsing
+	// init philos
+	// ^ or standalone: init fork and threads
+	// philo loop
 }
+
+// set"ter" get"ter" functions to set/get mutex status
+// write lock is a mutex
+
+// memset, printf, malloc, free, write,
+// usleep, gettimeofday, pthread_create,
+// pthread_detach, pthread_join, pthread_mutex_init,
+// pthread_mutex_destroy, pthread_mutex_lock,
+// pthread_mutex_unlock
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,11 +6,24 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:18:16 by mring             #+#    #+#             */
-/*   Updated: 2025/04/10 14:18:44 by mring            ###   ########.fr       */
+/*   Updated: 2025/04/11 15:47:39 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct philo
+{
+	int	eattime;
+	int	sleeptime;
+	int	deathtime;
+}		philo;
+
+int		ft_atoi(char *s);
+void	parse_pre_init(char **av);
 
 #endif
